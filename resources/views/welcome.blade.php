63f0ad5a469a72c6521c5dbe111f370c237844d3
YASSINE ZAOUALI <yassine.za.90@gmail.com>
2019-03-05 17:42:25 +0100

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel aaaaa
                 </div>
 
                 <div class="links">
